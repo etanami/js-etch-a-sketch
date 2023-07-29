ed354a2158934f6974e81ea439ce57c06a2f203c
Etanami Olatunji <etanamiolatunji@gmail.com>
2023-07-29 18:35:42 +0100

    feat: reset button to create new user custom grid

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 
 const gridContainer = document.querySelector("#grid-container");
+const squaresBtn = document.querySelector(".squares");
+const resetBtn = document.querySelector(".reset");
 
 const createSquareDiv = () => {
   const newDiv = document.createElement("div");
@@ -8,24 +10,44 @@ const createSquareDiv = () => {
   return newDiv;
 }
 
+// create grid function
 const createGrid = (num) => {
+  gridContainer.innerHTML = '';
   for (let i = 0; i < num; i++) {
     for (let j = 0; j < num; j++) {
       const squareDiv = createSquareDiv();
       gridContainer.appendChild(squareDiv);
     }
   }
+
+  gridContainer.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${num}, 1fr)`;
 }
 
+// default grid
 createGrid(16);
 
-document.addEventListener('DOMContentLoaded', () => {
-  const boxes = document.querySelectorAll(".box");
 
-  boxes.forEach(box => {
-    box.addEventListener('mouseover', () => {
-      box.style.backgroundColor = 'black'; 
-    });
-  });
+// event listener to change background color on hover using event delegation
+gridContainer.addEventListener('mouseover', (e) => {
+  e.preventDefault();
+  if (e.target.classList.contains('box')) {
+    e.target.style.backgroundColor = 'black';
+  }
+});
+
+resetBtn.addEventListener("click", e => {
+  e.preventDefault();
+  const squares = parseInt(prompt("How many squares per side would you like?"));
+
+  if (squares <= 100) {
+
+    changeGridSize(squares);
+  } else {
+    alert("Enter value betweeen 1 and 100");
+  }
 })
 
+const changeGridSize = (num) => {
+  createGrid(num);
+}
